af65dcec9c14a3ba32c98da9af808a1f4a41cec4
Ayyuce Demirbas <a.ayyuced@gmail.com>
2024-01-04 03:01:53 +0300

    Create txt_to_parquet.py

---
+++ txt_to_parquet.py
@@ -0,0 +1,11 @@
+#pip install pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import split, col
+
+spark = SparkSession.builder.appName('txt_to_parquet').getOrCreate()
+df = spark.read.text("klingon.txt")
+
+df = df.withColumn("text", split(col("value"), ",")[0])\
+       .drop("value")
+
+df.write.parquet("klingon.parquet")
